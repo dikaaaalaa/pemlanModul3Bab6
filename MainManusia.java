e331bd552eb3ab2e017dd65c4c4fc15f2b61b865
kalaa <164788792+dikaaaalaa@users.noreply.github.com>
2026-03-31 19:43:56 +0700

    Main class Manusia

--- MainManusia.java
+++ MainManusia.java
@@ -1,12 +1,56 @@
+import java.time.LocalDate;
+
 public class MainManusia {
     public static void main(String[] args) {
-        Manusia m1= new Manusia("Arab", true, "1234567890", true);
+        //contoh manusia
+        //Manusia m1= new Manusia("Arab", true, "1234567890", true);
+       //System.out.println(m1);
+
+       //System.out.println("=====================================");
+
+        //Manusia m2= new Manusia("Fuji", false, "0987654321", true);
+        //System.out.println(m2);
+
+        //System.out.println("=====================================");
+
+        //Manusia m3= new Manusia("Furab", true, "1122334455", false);
+        //System.out.println(m3);
+
+        //contoh mahasiswa
+        //System.out.println("=====================================");
+
+        //MahasiswaFILKOM m4 = new MahasiswaFILKOM("Aseep", true, "1122334455", false, "165150300111100", 2.5);
+        //System.out.println(m4);
+
+        //System.out.println("=====================================");
+
+        //MahasiswaFILKOM m5 = new MahasiswaFILKOM("Osoop", false, "9988776655", false, "255150700111200", 3.2);
+        //System.out.println(m5);
+
+        //System.out.println("=====================================");
+
+        //MahasiswaFILKOM m6 = new MahasiswaFILKOM("Usuup", true, "5566778899", false, "215150400111300", 3.8);
+        //System.out.println(m6);
+        //contoh pekerja
+       // System.out.println("=====================================");
+
+        //Pekerja p1 = new Pekerja("Kasep", true, "111", true, 1000, LocalDate.of(2024,3, 2), 2);
+        //System.out.println(p1);
+
+        //System.out.println("=====================================");
+
+        //Pekerja p2 = new Pekerja("Udiin", false, "222", false, 1500, LocalDate.of(2017,1, 1),0);
+        //System.out.println(p2);
+
+        //System.out.println("=====================================");
+
+        //Pekerja p3 = new Pekerja("Dudung", true, "333", true, 2000, LocalDate.of(2006,1, 1), 10);
+        //System.out.println(p3);
+
+        //contoh manager
+        System.out.println("=====================================");
+        Manager m1 = new Manager("Syifadanantara", true, "333", true,7500, LocalDate.of(2011,1, 2), 3, "HRD");
         System.out.println(m1);
-
-        Manusia m2= new Manusia("Fuji", false, "0987654321", true);
-        System.out.println(m2);
-
-        Manusia m3= new Manusia("Furab", true, "1122334455", false);
-        System.out.println(m3);
     }
 }
+
